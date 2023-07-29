08d2b3f96f5bc24242de7f250907e50639da441c
joaomcsferreira <marcosc024@gmail.com>
2023-07-28 21:07:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting Started
+
+A data handling middleware facilitating communication between the front-end and the PokeAPI website.
---
+++ src/main/java/com/joaomarcos/pokeapimiddleware/PokeApiMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.joaomarcos.pokeapimiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiMiddlewareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeApiMiddlewareApplication.class, args);
+    }
+
+}
